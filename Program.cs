95b0868d20d8738e9ed1b5e8f590ba96960f1d65
Award505 <148552072+Award505@users.noreply.github.com>
2024-10-02 07:53:04 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.Serialization;
+
+namespace APP
+{
+    class Program
+    {
+        static void Main()
+        {
+            // ИДЗ 1 Задача 16 уровень 1
+            // Console.WriteLine("Введите трёхзначное число");
+            // int x = Convert.ToInt32(Console.ReadLine());
+            // if (x >= 100 && x <= 999){
+            //     string xstr = x.ToString();
+            //     string newxstr = xstr[1].ToString() + xstr[0].ToString() + xstr[2].ToString();
+            //     int newx = Convert.ToInt32(newxstr);
+            //     Console.WriteLine(newx);
+            //     Console.ReadLine();
+            //}
+
+
+            // ИДЗ 1 Задача 16 уровень 2
+            // Console.WriteLine("Введите четырехзначное число");
+            // int x = Convert.ToInt32(Console.ReadLine());
+            // if (x >= 1000 && x <= 9999)
+            // {
+            //     string xstr = x.ToString();
+            //     string newxstr = xstr[1].ToString() + xstr[0].ToString() + xstr[3].ToString() + xstr[2].ToString();
+            //     int newx = Convert.ToInt32(newxstr);
+            //     Console.WriteLine(newx);
+            // }
+
+
+        //     ИДЗ 2 Задача 16 уровень 1
+        //     Console.WriteLine("Введите день года");
+        //     int k = Convert.ToInt32(Console.ReadLine());
+        //     if (k >= 1 && k<=365)
+        //     {
+        //      int dow = (k - 1) % 7;
+        //         if (dow == 5 || dow == 6)
+        //         {
+        //             Console.WriteLine($"День {k} выходной");
+        //         }
+        //         else
+        //         {
+        //             Console.WriteLine($"День {k} рабочий");
+        //         }
+        //    } 
+
+        
+
+            
+            
+
+        }
+    }
+}
